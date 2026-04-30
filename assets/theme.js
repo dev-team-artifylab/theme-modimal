26efca24252379942fce6cc59d179e5fb58e079c
Tawhid Shakil <astalukdar39@gmail.com>
2026-04-30 14:30:26 +0600

    design filter label as per as figma

--- assets/theme.js
+++ assets/theme.js
@@ -2,18 +2,38 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.filter-toggle').forEach(btn => {
     const content = btn.nextElementSibling;
     const icon = btn.querySelector('.icon');
+    const parent = btn.closest('.filter-item');
 
     // initial state
-    if (icon && !content.classList.contains('hidden')) {
+    if (content && !content.classList.contains('hidden')) {
       icon.textContent = '−';
+
+      parent.classList.add('border', 'border-[#748C70]', 'p-2');
+
+      btn.classList.remove('bg-[#748C70]', 'text-white');
+      btn.classList.add('bg-transparent', 'text-[#748C70]');
     }
 
-    // click event
     btn.addEventListener('click', () => {
       content.classList.toggle('hidden');
 
-      if (icon) {
-        icon.textContent = content.classList.contains('hidden') ? '+' : '−';
+      const isHidden = content.classList.contains('hidden');
+
+      // icon
+      icon.textContent = isHidden ? '+' : '−';
+
+      if (isHidden) {
+        // CLOSED
+        parent.classList.remove('border', 'border-[#748C70]', 'p-2');
+
+        btn.classList.add('bg-[#748C70]', 'text-white');
+        btn.classList.remove('bg-transparent', 'text-[#748C70]');
+      } else {
+        // OPEN
+        parent.classList.add('border', 'border-[#748C70]', 'p-2');
+
+        btn.classList.remove('bg-[#748C70]', 'text-white');
+        btn.classList.add('bg-transparent', 'text-[#748C70]');
       }
     });
   });
